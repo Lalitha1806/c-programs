1626e514800502dfe6ca820fad0670e777619fab
Lalitha1806 <lalithaprasannakunapareddy@gmail.com>
2024-10-25 12:00:38 +0530

    Create Simpleif.c

---
+++ Simpleif.c
@@ -0,0 +1,11 @@
+// check given number is positive 
+#include <stdio.h>
+int main() {
+    int n;
+    printf("enter the n:");
+    scanf("%d",&n);
+    if(n>0){
+        printf("positive ");
+    }
+    return 0;
+}
