b2f5e3e0f1c6e3c2e5c4b466e73ab473925b04e1
Lalitha1806 <lalithaprasannakunapareddy@gmail.com>
2024-10-29 19:47:50 +0530

    Create If.c

---
+++ If.c
@@ -0,0 +1,13 @@
+// check whether the given number is positive
+#include <stdio.h>
+int main() 
+{
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("positive ");
+    }
+    return 0;
+}
