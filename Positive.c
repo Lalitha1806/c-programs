03dadae8649755d1c269cea676a2b32c1ce172ba
Lalitha1806 <lalithaprasannakunapareddy@gmail.com>
2024-10-29 19:46:39 +0530

    Create Positive.c

---
+++ Positive.c
@@ -0,0 +1,17 @@
+// check whether the given number is positive or negative 
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("positive ");
+    }
+    else
+    {
+        printf("Negative ");
+    }
+    return 0;
+}
